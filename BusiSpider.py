4770073c2e3c8526aeadecb146eb03583a451ba8
kennethzhong <webberzsj@163.com>
2019-01-28 16:33:20 +0800

    new s

--- BusiSpider.py
+++ BusiSpider.py
@@ -3,6 +3,7 @@
 
 import requests
 import re
+import time
 from requests.exceptions import RequestException
 
 from selenium import webdriver
@@ -10,6 +11,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.select import Select
 
 
 #browser =  webdriver.PhantomJS(executable_path = r'D:\tool\phantomjs-2.1.1-windows\phantomjs-2.1.1-windows\bin\phantomjs.exe')
@@ -22,12 +24,36 @@ base_url = u'http://gd.122.gov.cn'
 detail_url = u'http://gd.122.gov.cn/publicitypage?size=20&page=1&tjyf=201810&fzjg=%E7%B2%A4E&fwdmgl=6003'
 
 def search(url):
-    browser =  webdriver.Chrome('D:\\tool\\chromedriver_win32\\chromedriver.exe')
+    option = webdriver.ChromeOptions()
+    option = option.add_argument('disable-infobars')
+    browser =  webdriver.Chrome('D:\\tool\\chromedriver_win32\\chromedriver.exe', chrome_options=option)
     wait =  WebDriverWait(browser, 10)
     browser.get(url)
     print browser.page_source.encode('utf8')
+
+    ul = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,'#notice-search-bar > div.notice-search > ul')))
+    lis = ul.find_elements_by_xpath('li')
+    #time.sleep(1)
+    #print len(lis)
+    lis[-1].click()
+    time.sleep(1)
+
+    element = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,'#jxpxzl > div.select > select')))
+    Select(element).select_by_value(u'粤E')
+
+    input = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,'#jxpxzl > div:nth-child(3) > input')))
+    #print input.get_attribute("name")
+    #input.send_keys("11")
+    #input.clear()
+    input.send_keys(u'盈通达驾驶员培训有限公司')
+
+    input_captcha = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,'#jxpxzl > div.controls.controls-row.tipBox > input.ml-10.yzminput')))
+    input_captcha.click()
+
+    img_val =  wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,'#jxpxzl > div.controls.controls-row.tipBox > span > img')))
+    img_url = img_val.get_attribute('src')
+    print img_url
     browser.close()
-    #input = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR),'body > div > div:nth-child(3) > table'))
     return
 
 
@@ -118,15 +144,21 @@ def main():
     # print total_count
     #print html.encode('utf8')
 
-    ym_count = 0
-    ym_2018 = ['201801','201802','201803','201804','201805','201806','201807','201808','201809','201810','201811','201812']
-    ym_2017 = ['201701','201702','201703','201704','201705','201706','201707','201708','201709','201710','201711','201712']
-    ym_2016 = ['201601','201602','201603','201604','201605','201606','201607','201608','201609','201610','201611','201612']
-    ym_2015 = ['201501','201502','201503','201504','201505','201506','201507','201508','201509','201510','201511','201512']
-    for x in ym_2018 :
-        ym_count += get_ym_count(x)
-        print x,ym_count
-    print ym_count
+
+    ##########get registered drivers
+    # ym_count = 0
+    # ym_2018 = ['201801','201802','201803','201804','201805','201806','201807','201808','201809','201810','201811','201812']
+    # ym_2017 = ['201701','201702','201703','201704','201705','201706','201707','201708','201709','201710','201711','201712']
+    # ym_2016 = ['201601','201602','201603','201604','201605','201606','201607','201608','201609','201610','201611','201612']
+    # ym_2015 = ['201501','201502','201503','201504','201505','201506','201507','201508','201509','201510','201511','201512']
+    # for x in ym_2018 :
+    #     ym_count += get_ym_count(x)
+    #     print x,ym_count
+    # print ym_count
+
+    url = u'http://gd.122.gov.cn/views/notice.html'
+    html = search(url)
+    print html
 
     # pattern = re.compile('<td title.*?</td>.*?<td>(.*?)</td>',re.S)
     # result = pattern.findall(html)
